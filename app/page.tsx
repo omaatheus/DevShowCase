ae56c4320c8253988f4627c10360d44891428867
Matheuss <matheusgida@outlook.com>
2025-01-09 15:59:54 -0300

    :sparkles: feat: button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Hero from "./components/landing-page/hero";
 
 export default function Home() {
-  return (
-    <div>
-      Hello World
+  return <div className="h-screen max-w-7xl mx-auto border">
+    
+     <Hero />
+      {/* <Header />
+      
+      <VideoExplanation />
+      <Pricing />
+      <FAQ /> */}
     </div>
-  );
+  
 }
