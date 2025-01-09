3cb9244c1c2e23e93fe2af58a69ba9fc0d817c37
Matheuss <matheusgida@outlook.com>
2025-01-09 17:46:52 -0300

    :sparkles: feat: header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return <div className="h-screen max-w-7xl mx-auto">
-    
+    <Header />
      <Hero />
-      {/* <Header />
-      
+       
+      {/*
       <VideoExplanation />
       <Pricing />
       <FAQ /> */}
