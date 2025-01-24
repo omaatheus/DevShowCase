3b5b849591c55426491107c639777726c627f968
Matheuss <matheusgida@outlook.com>
2025-01-24 18:14:44 -0300

    :sparkles: feat: faq component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
+import FAQ from "./components/landing-page/faq";
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
+import Pricing from "./components/landing-page/pricing";
+import VideoExplanation from "./components/landing-page/videoExplanation";
 
 export default function Home() {
   return <div className="h-screen max-w-7xl mx-auto">
     <Header />
      <Hero />
        
-      {/*
+      
       <VideoExplanation />
+      
       <Pricing />
-      <FAQ /> */}
+
+      <FAQ />
     </div>
   
 }
