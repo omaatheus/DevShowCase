995566fed1852adf171826b13219d62a7244ec2c
Matheus <matheusgida@outlook.com>
2026-02-23 21:22:06 -0300

    feat: copyright year

--- app/components/landing-page/footer.tsx
+++ app/components/landing-page/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
       <a href="https://www.instagram.com/sejafluxo/" rel="noopener noreferrer" target="_blank">
       </a>
     </a>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2025 Powered by Fluxo —
+    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© {new Date().getFullYear()} Powered by Fluxo —
       <a href="https://www.instagram.com/linkshowcase/" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@linkshowcase</a>
     </p>
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
